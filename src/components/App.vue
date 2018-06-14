5f28c2c6a837222a9dce0d3f2a53f74539153fba
Harry Park <harry@harrypark.io>
2018-06-14 14:24:22 +1000

    rm log + linter fixes

--- src/components/App.vue
+++ src/components/App.vue
@@ -17,7 +17,7 @@
 			return {
 				content: ['# ghmd', 'an accurate github flavoured markdown preview', '```js', 'let a = 1;', 'let b = 2;', '', 'console.log(a + b) // 3', '```'].join('\n'),
 				filename: 'README.md'
-			}
+			};
 		},
 		created() {
 			if (localStorage.Markdown) {
@@ -38,7 +38,7 @@
 					const rawContents = await fetch(window.URL.createObjectURL(fileObj));
 					const reader = rawContents.body.getReader();
 					const uint8 = await reader.read();
-					const data = new TextDecoder("utf-8").decode(uint8.value);
+					const data = new TextDecoder('utf-8').decode(uint8.value);
 					this.content = data;
 					this.save();
 				} catch(err) {
@@ -52,7 +52,7 @@
 				if (!content) {
 					content = this.content;
 				}
-				localStorage.Markdown = JSON.stringify({ filename, content});
+				localStorage.Markdown = JSON.stringify({ filename, content });
 			}
 		},
 		components: {
--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -46,5 +46,5 @@
 				default: ''
 			}
 		}
-	}
+	};
 </script>
